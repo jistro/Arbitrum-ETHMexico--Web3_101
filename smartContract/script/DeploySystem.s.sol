c1ab1a44284f8e7bbb7e73e3dceaa75bb4ace7e1
Kevin Padilla <jistro@skiff.com>
2023-10-20 11:30:45 -0600

    update

--- smartContract/script/DeploySystem.s.sol
+++ smartContract/script/DeploySystem.s.sol
@@ -2,14 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import { Data } from "../src/Data.sol";
-import { Service } from "../src/Service.sol";
+import {Data} from "../src/Data.sol";
+import {Service} from "../src/Service.sol";
 
-contract DeploySystemLocalScript is Script {
+contract DeploySystemScript is Script {
     Data data;
     Service service;
 
-    address adminAddress = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address adminAddress = 0xF11f8301C76F46733d855ac767BE741FFA9243Bd;
+    string firstName = "Kevin";
+    string lastName = "Padilla";
 
     function run() public {
         vm.startBroadcast(adminAddress);
@@ -17,11 +19,11 @@ contract DeploySystemLocalScript is Script {
         service = new Service(
             address(data), 
             adminAddress,
-            "Cosme",
-            "Fulanito de Tal"
+            firstName,
+            lastName
             );
-        (bool infoAdm, address admAdd)=data.grantAdminRole(address(service));
-        (bool infodata, address dataAdd)=data.grantDataProviderRole(address(service));
+        (bool infoAdm, address admAdd) = data.grantAdminRole(address(service));
+        (bool infodata, address dataAdd) = data.grantDataProviderRole(address(service));
         console2.log("Data address: ", address(data));
         console2.log("Service address: ", address(service));
         console2.log("Admin address: ", admAdd);
